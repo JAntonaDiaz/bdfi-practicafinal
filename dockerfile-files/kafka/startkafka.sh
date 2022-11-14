cf1974e585c97023a479a654ea75b149d1de0b34
AEncinas19 <user1@instance-bdfi-proyecto-final.europe-southwest1-a.c.bdfi-367411.internal>
2022-11-14 10:45:56 +0000

    Modificacion dockerkafka 3.0

--- dockerfile-files/kafka/startkafka.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#./../../../kafka_2.12-3.1.2/bin/kafka-server-start.sh config/server.properties &
-
-#./../../../kafka_2.12-3.1.2/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request
-
-./bin/kafka-server-start.sh config/server.properties
