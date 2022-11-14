637331a3643b878c42e1134de3ecea9dafba7fd2
root <root@instance-bdfi-proyecto-final.europe-southwest1-a.c.bdfi-367411.internal>
2022-11-14 11:29:23 +0000

    Last try

--- scripts/startkafka.sh
+++ scripts/startkafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-./bin/kafka-server-start.sh config/server.properties 
+sleep 7;
+./bin/kafka-server-start.sh config/server.properties & sleep 10;
 
 ./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request
 
