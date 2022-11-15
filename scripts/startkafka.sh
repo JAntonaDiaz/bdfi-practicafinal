956b33a7b8daf16a3447da931d94b1aef55a0e69
AEncinas19 <user1@instance-bdfi-proyecto-final.europe-southwest1-a.c.bdfi-367411.internal>
2022-11-15 12:03:20 +0000

    Docker-compose v1.0

--- scripts/startkafka.sh
+++ scripts/startkafka.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sleep 7;
-./bin/kafka-server-start.sh config/server.properties & sleep 10;
-
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request
+./bin/kafka-server-start.sh config/server.properties & sleep 10 && ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request
 
 #./bin/kafka-server-start.sh config/server.properties
