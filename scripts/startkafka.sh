4ae055415d53183cfb3532a70222e6e35c928018
AEncinas19 <user1@instance-bdfi-proyecto-final.europe-southwest1-a.c.bdfi-367411.internal>
2022-11-16 10:49:32 +0000

    Solo falta el mongo

--- scripts/startkafka.sh
+++ scripts/startkafka.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-./bin/kafka-server-start.sh config/server.properties & sleep 10 && ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request
+./bin/kafka-server-start.sh config/server.properties 
+
+#& sleep 10 && ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request & sleep 12345
 
 #./bin/kafka-server-start.sh config/server.properties
