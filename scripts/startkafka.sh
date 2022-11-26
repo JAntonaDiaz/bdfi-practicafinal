d33049087ab654180093f913ae0ec6bd470ae8d0
AEncinas19 <user1@instance-bdfi-proyecto-final.europe-southwest1-a.c.bdfi-367411.internal>
2022-11-26 23:54:45 +0000

    Actualizar startkafka.sh

--- scripts/startkafka.sh
+++ scripts/startkafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 /main/bdfi-practicafinal/scripts/mod-kafka.py & sleep 15 
+python3 ../bdfi-practicafinal/scripts/mod-kafka.py & sleep 15 
 ./bin/kafka-server-start.sh config/server.properties & sleep 15 & ./bin/kafka-topics.sh --create --bootstrap-server $KAFKA_HOST:$KAFKA_PORT --replication-factor 1 --partitions 1 --topic flight_delay_classification_request & sleep infinity
 #& sleep 10 && ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request & sleep 12345
