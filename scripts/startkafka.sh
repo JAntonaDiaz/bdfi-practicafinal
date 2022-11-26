0a372690760ef2f94923cd24fcbc631324d35c79
AEncinas19 <user1@instance-bdfi-proyecto-final.europe-southwest1-a.c.bdfi-367411.internal>
2022-11-26 16:06:12 +0000

    Otra prueba

--- scripts/startkafka.sh
+++ scripts/startkafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ../bdfi-practicafinal/scripts/mod-kafka.py & sleep 15 & ./bin/kafka-server-start.sh config/server.properties 
+python3 mod-kafka.py & sleep 15 & ../../kafka_2.12-3.1.2/bin/kafka-server-start.sh config/server.properties 
 
 #& sleep 10 && ./bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic flight_delay_classification_request & sleep 12345
